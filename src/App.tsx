6ae8dca7ef518642dfc7b86c0a5b2b4a2ff0d036
pakli <pakli991088@gmail.com>
2023-09-15 17:43:42 +0800

    asfsafPlease enter the commit message for your changes. Lines starting

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,30 @@ import './App.css';
 import {Navbar} from "./layouts/NavbarAndFooter/Navbar";
 import {Footer} from "./layouts/NavbarAndFooter/Footer";
 import {HomePage} from "./layouts/HomePage/HomePage";
+import {SearchBookPage} from "./layouts/SearchBookPage/SearchBookPage";
+import {Route, Switch} from "react-router-dom";
+import {BookCheckoutPage} from "./layouts/BookCheckoutPage/BookCheckoutPage";
 
 export const App = () => {
     return (
-        <div>
+        <div className='d-flex flex-column min-vh-100'>
             <Navbar/>
-            <HomePage/>
+            <div className='flex-grow-1'>
+                <Switch>
+                    <Route path='/' exact>
+                        <HomePage/>
+                    </Route>
+                    <Route path='/home'>
+                        <HomePage/>
+                    </Route>
+                    <Route path='/search'>
+                        <SearchBookPage/>
+                    </Route>
+                    <Route path='/checkout/:bookId'>
+                        <BookCheckoutPage/>
+                    </Route>
+                </Switch>
+            </div>
             <Footer/>
         </div>
     );
